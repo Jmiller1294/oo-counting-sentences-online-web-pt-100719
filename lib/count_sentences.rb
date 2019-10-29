93e36cb2f32b08ef436a135e0202603842ae4f5f
Justin Miller <jayman1294@gmail.com>
2019-10-29 07:00:45 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,32 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".") 
   end
 
   def question?
-
+    if self.end_with?("?")
+      true 
+    else
+      false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      true 
+    else
+      false
+    end
   end
 
   def count_sentences
-
-  end
+     new_array = []
+     if self.end_with?(".") 
+        self.split()
+     end
+    
+  end 
+ 
 end
+    
